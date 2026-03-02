e1556cb0db1832db4cd0c408cdc23114f45cfb1b
theoskold <theo.skold@hotmail.com>
2026-03-02 10:57:20 +0100

    1

--- Program.cs
+++ Program.cs
@@ -1,13 +1,33 @@
-﻿namespace Lösenordshanterare
+﻿using System.IO;
+
+
+
+namespace Lösenordshanterare
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!!!!!");
-            // Hej!
-            //Test igen!
+            Console.WriteLine("Ange sökväg för klientfil (t.ex. client.txt):");
+            string clientPath = Console.ReadLine();
+
+            Console.WriteLine("Ange sökväg för serverfil (t.ex. server.txt):");
+            string serverPath = Console.ReadLine();
+
+
+            if (!File.Exists(clientPath))
+            {
+                File.WriteAllText(clientPath, ""); // Skapar en tom fil
+                [cite_start] Console.WriteLine($"Klientfil skapad på: {clientPath}"); [cite: 150]
+}
+
+            // Skapa serverfilen om den inte finns
+            if (!File.Exists(serverPath))
+            {
+                [cite_start] File.WriteAllText(serverPath, ""); [cite: 138]
+    Console.WriteLine($"Serverfil skapad på: {serverPath}");
+            }
+
         }
     }
 }
