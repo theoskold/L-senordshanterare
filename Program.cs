8add78c7bd133df3d7c214c3200ce0154c0b7117
theoskold <theo.skold@hotmail.com>
2026-03-02 10:40:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lösenordshanterare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!!!!!");
+        }
+    }
+}
